fc8e8e0cde63ec0cd695e1e785bf1de5f4763f25
Parvez Belim <parvez.belim@rku.ac.in>
2023-12-30 17:52:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# icdtsb-conf
+1st International Conference on Digital Transformation & Sustainability of Business 
---
+++ viewpdf.php
@@ -0,0 +1,15 @@
+<?php
+$file = './assets/documents/CONFERENCE BROCHURE final.pdf';
+
+// Check if the file exists
+if (file_exists($file)) {
+    // Set the Content-Type header to application/pdf
+    header('Content-Type: application/pdf');
+
+    // Read and output the file content
+    readfile($file);
+} else {
+    // Handle the case when the file does not exist
+    echo 'File not found: ' . $file;
+}
+?>
